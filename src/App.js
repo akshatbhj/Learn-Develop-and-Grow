03c56cbb23cda41db86956676b23378ce069acc2
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2023-07-15 10:37:34 +0530

    Courses Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Courses from "./components/Courses";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/courses" element={<Courses />}></Route>
           <Route path="/signin" element={<SignIn />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
---
+++ src/components/Courses.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { MdAccountBox } from "react-icons/md";
+
+const Courses = () => {
+  return (
+    <div className="w-full pt-[12rem] pb-[2rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full flex flex-col p-4 my-4 rounded-lg">
+          <h1 className="text-5xl font-bold text-center py-8 md:my-[4rem] my-[2rem]">
+            Check out our <span className="text-[#f2be22]">courses</span>
+          </h1>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <MdAccountBox
+            width={100}
+            fontSize={75}
+            className="mx-auto mt-[-3rem] bg-white"
+          />
+          <h1 className="text-3xl font-bold text-center py-8">Standard</h1>
+          <p className="text-4xl text-center font-bold">Rs 3000/-</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">
+              Unlock your full potential and excel in personal and professional
+              growth.
+            </p>
+            <p className="py-2 border-b mx-8">Duration 6 months</p>
+          </div>
+          <button className="bg-[#f2be22] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-black">
+            Enroll Now
+          </button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <MdAccountBox
+            width={100}
+            fontSize={75}
+            className="mx-auto mt-[-3rem] bg-white"
+          />
+          <h1 className="text-3xl font-bold text-center py-8">Premium</h1>
+          <p className="text-4xl text-center font-bold">Rs 6000/-</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">
+              Unlock your full potential and excel in personal and professional
+              growth.
+            </p>
+            <p className="py-2 border-b mx-8">Duration 12 months</p>
+          </div>
+          <button className="bg-[#f2be22] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-black">
+            Enroll Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Courses;
