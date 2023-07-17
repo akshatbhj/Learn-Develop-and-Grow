b5d1174c29748721ada76a1dd6643ee776b012d8
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2023-07-17 16:47:58 +0530

    Modifying courses section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,7 +29,8 @@ const Home = () => {
           ></video>
           <div className="flex flex-col justify-center items-center">
             <p className="text-white font-bold lg:text-4xl lg:p-8 md:text-3xl md:text-center lg:text-left sm:text-3xl text-2xl py-2 mt-4 md:mt-2 mx-4 lg:mx-auto">
-              UNLEASH & EMPOWER YOUR TRUE POTENTIAL WITH LEARN DEVELOP &{" "} <span className="text-[red] font-extrabold" >GROW</span>
+              UNLEASH & EMPOWER YOUR TRUE POTENTIAL WITH LEARN DEVELOP &{" "}
+              <span className="text-[red] font-extrabold">GROW</span>
             </p>
             <p className="font-bold text-gray-500 lg:pb-4 md:text-xl text-2xl md:ml-8 mx-4 mt-8 md:mt-2 sm:mx-8">
               Speed up the skill acquisition process by finding unlimited
@@ -46,12 +47,11 @@ const Home = () => {
 
       <div className="w-full pt-[6rem] pb-[2rem] px-4 bg-white">
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 gap-">
-          <div className="w-full flex flex-col p-4 my-2 rounded-lg">
-            <h1 className="text-5xl font-bold text-left py-2 md:my-[2rem] my-[1rem] leading-tight">
-              Unleash your potential with{" "}
-              <span className="text-[#f2be22] ">Learn Develop Grow.</span>
+          <div className="w-full flex flex-col p-4 my-2 rounded-lg text-left ">
+            <h1 className="text-4xl font-bold text-left py-2 md:my-[2rem] my-[1rem] mt-[-2rem] lg:text-5xl leading-tight">
+              Enhance communication, leadership, and self-confidence.
             </h1>
-            <p className="font-bold text-gray-500 text-xl">
+            <p className="font-bold text-gray-500 text-xl mt-[1rem] ">
               Our site offers the inspiration and resources you need to develop
               yourself and reach your goals.
             </p>
@@ -63,7 +63,7 @@ const Home = () => {
               />
               <button
                 type="submit"
-                class="ml-2 px-4 py-2 rounded-r-lg bg-[#f2be22] font-bold text-black hover:opacity-80"
+                class="ml-2 px-4 py-2 rounded-r-lg bg-[red] font-bold text-white hover:opacity-80"
               >
                 Submit
               </button>
@@ -79,17 +79,20 @@ const Home = () => {
         </div>
       </div>
       <div className="w-full pt-[6rem] pb-[2rem] px-4 bg-white">
-        <h1 className="text-[#000300] text-5xl md:text-5xl lg:text-6xl font-bold text-center mb-[5rem]">
-          Check out our <span className="text-[#f2be22]">courses.</span>
+        <h1 className="text-[#000300] text-5xl md:text-5xl lg:text-6xl font-bold text-center mt-[-2rem] mb-[8rem]">
+          Check out our <span className="text-[red]">courses.</span>
         </h1>
-        <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 gap-8">
           <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <img src={homeImg} alt="/" />
-          </div>
-          <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <img src={logo} alt="/" className="mx-auto mt-[-3rem] bg-white w-20 h-20 rounded-full" />
+            <img
+              src={logo}
+              alt="/"
+              className="mx-auto mt-[-3rem] bg-white w-20 h-20 rounded-full"
+            />
             <h1 className="text-3xl font-bold text-center py-8">Standard</h1>
-            <p className="text-4xl text-center font-bold">Rs 3000/-</p>
+            <p className="text-4xl text-center font-bold">
+              Rs 500 /- <span className="text-[16px]">per month</span>
+            </p>
             <div className="text-center font-medium">
               <p className="py-2 border-b mx-8 mt-8">
                 Unlock your full potential and excel in personal and
@@ -97,16 +100,23 @@ const Home = () => {
               </p>
               <p className="py-2 border-b mx-8">Duration 6 months</p>
               <NavLink to="/signup">
-                <button className="bg-[#f2be22] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-black">
+                <button className="bg-[red] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-white">
                   Enroll Now
                 </button>
               </NavLink>
             </div>
           </div>
           <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <img src={logo} alt="/" className="mx-auto mt-[-3rem] bg-white w-20 h-20 rounded-full" />
+            <img
+              src={logo}
+              alt="/"
+              className="mx-auto mt-[-3rem] bg-white w-20 h-20 rounded-full"
+            />
             <h1 className="text-3xl font-bold text-center py-8">Premium</h1>
-            <p className="text-4xl text-center font-bold">Rs 6000/-</p>
+            <p className="text-4xl text-center font-bold">
+              Rs 500 /- <span className="text-[16px]">per month</span>
+            </p>
+
             <div className="text-center font-medium">
               <p className="py-2 border-b mx-8 mt-8">
                 Unlock your full potential and excel in personal and
@@ -114,7 +124,7 @@ const Home = () => {
               </p>
               <p className="py-2 border-b mx-8">Duration 12 months</p>
               <NavLink to="/signup">
-                <button className="bg-[#f2be22] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-black">
+                <button className="bg-[red] w-[150px]  md:w-[180px] rounded-md my-6 mx-auto font-medium px-6 py-3 text-white">
                   Enroll Now
                 </button>
               </NavLink>
